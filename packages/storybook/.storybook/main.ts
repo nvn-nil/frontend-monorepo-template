cea153ad7432a7e53ee3c1da173aebd8081d625a
Naveen Anil <naveen.anil@wind-pioneers.com>
2023-10-05 20:32:04 +0530

    OPS: Setup storybook

---
+++ packages/storybook/.storybook/main.ts
@@ -0,0 +1,26 @@
+import { dirname, join } from "path";
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+
+  "addons": [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+
+  "framework": {
+    name: getAbsolutePath("@storybook/react-webpack5"),
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
+}
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
